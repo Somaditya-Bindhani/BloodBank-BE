482de575209b269e46cfb801485ce80006643e96
Somaditya-Bindhani <somaditya.bindhani136@gmail.com>
2022-09-04 04:42:55 +0530

    added admin fucntionality

--- router/router.js
+++ router/router.js
@@ -6,9 +6,45 @@ const checkInvalidInput = require("../middlewares/invalidPost");
 
 router.post(
   "/login",
-  [body("userId").notEmpty(), body("password").isLength({ min: "6" })],
+  [body("email").notEmpty(), body("password").isLength({ min: "6" })],
   checkInvalidInput.checkError,
   authController.login
+);
+router.post(
+  "/createOrgAdmin",
+  [body("email").notEmpty()],
+  checkInvalidInput.checkError,
+  authController.createOrgAdmin
+);
+router.post(
+  "/createOrganization",
+  [
+    body("name").notEmpty(),
+    body("address").notEmpty(),
+    body("state").notEmpty(),
+    body("city").notEmpty(),
+    body("PIN").isNumeric().isLength({ min: 6, max: 6 }),
+    body("contactNumber").isNumeric().isLength({ min: 10, max: 10 }),
+  ],
+  checkInvalidInput.checkError,
+  authController.createOrganization
+);
+router.post(
+  "/linkOrgAdmin",
+  [
+    body("orgAdminId").notEmpty().isMongoId(),
+    body("orgId").notEmpty().isMongoId()
+  ],
+  checkInvalidInput.checkError,
+  authController.linkOrgAdmin
+);
+router.post(
+  "/activateOrgAdmin",
+  [
+    body("orgAdminId").notEmpty().isMongoId(),
+  ],
+  checkInvalidInput.checkError,
+  authController.activateOrgAdmin
 );
 
 module.exports = router;
