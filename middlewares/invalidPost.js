e54baabb5a669e46be6a28622bb2cf61ad76936f
MD-ARBAB-SAHID <arbabsahid7@gmail.com>
2022-10-11 16:21:23 +0530

    changes

--- middlewares/invalidPost.js
+++ middlewares/invalidPost.js
@@ -3,7 +3,7 @@ const { validationResult } = require("express-validator");
 const checkError = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(402).json("Invalid Inputs!");
+    return res.status(400).json("Invalid Inputs!");
   }
   next();
 };
