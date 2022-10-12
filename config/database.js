afb13d0fd0104cfd8aedbb7a3477c104d3b96f3c
Somaditya-Bindhani <somaditya.bindhani136@gmail.com>
2022-10-13 01:09:42 +0530

    CHANGE : vercel try

--- config/database.js
+++ config/database.js
@@ -2,15 +2,16 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 const connect = async () => {
-  try {
-    mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      autoIndex: false,
-    });
-    console.log("Database Connected.");
-  } catch (err) {
-    console.log(err);
-  }
+  console.log(process.env.MONGO_URL);
+  //   try {
+  //     await mongoose.connect(process.env.MONGO_URL, {
+  //       useNewUrlParser: true,
+  //       useUnifiedTopology: true,
+  //       autoIndex: false,
+  //     });
+  //     console.log("Database Connected.");
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
 };
 module.exports.connect = connect;
