8a89e0c823bb16664dc2fcbd8fa375259cd3042d
Somaditya-Bindhani <somaditya.bindhani136@gmail.com>
2022-10-18 15:02:36 +0530

    CHANGE : user pass reset , data not accessible

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,4 +1,5 @@
 const rules = require("../data/rule");
+const bypass = require("../data/bypass");
 const OrgAdmin = require("../models/orgAdmin");
 const Workspace = require("../models/workspace");
 const HttpError = require("../models/http-error");
@@ -14,6 +15,14 @@ const authorization = async (req, res, next) => {
     workspace = await Workspace.find({ orgAdminId });
   } catch (err) {
     console.log(err);
+  }
+  if (!user.isReset) {
+    const flag = bypass.find(
+      (ele) => ele.route === allPath[2] && ele.role === user.role
+    );
+    if (!flag || !flag.bypass) {
+      return next(new HttpError("User Password Reset Required.", 400));
+    }
   }
   const access = rules[allPath[1]][allPath[2]].find(
     (ele) => ele.role === user.role
